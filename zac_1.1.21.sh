f651ef1a2285e6b8e0c530853b0ae64c4a8b521e
Serade12 <73163517+Serade12@users.noreply.github.com>
2021-02-05 08:52:03 +0800

    Update zac_1.1.21.sh

--- zac_1.1.21.sh
+++ zac_1.1.21.sh
@@ -2,6 +2,9 @@
 
 result=Yes
 echo " "
+#df command to check the file system disk space occupancy, permissions for all users. 2. Format 3. The main parameters -s: given only the total number of data blocks occupied by each paramet...
+#--local limit listing to local file systems
+#
 #2> /dev/null ensures the results is not printed out. 
 # -eq 0 checks that nothing is returned.
 if [ $(df --local -P | awk '{if (NR!=1) print$6}'| xargs -I '{}' find '{}' -xdev -type d \( -perm -002 -a ! -perm -1000 \) 2> /dev/null | wc -l) -eq 0 ]
